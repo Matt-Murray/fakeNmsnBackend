d8860c513298004820594dc23e2aff3e87510aaf
Clyde <cbates@rfdinc.com>
2020-01-06 14:36:23 -0600

    Mock data for refer

--- index.js
+++ index.js
@@ -6,14 +6,63 @@ const path = require('path');
 // app.use(forceSSL());
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+  });
 
 app.get('/NmsnrespRetrievalService', function(req, res) {
-  
+    
 });
 
 app.get('/NmsnreferRetrievalService', function(req, res) {
-    
+    res.status('200').json(
+        [
+            { 
+            //   docTrackingNum
+            nmsnReferralReportTypeCd: 'MN',
+                nmsnReferralStatusCd: 'ACCEPTED',
+                employerFein: '12121234',
+                employeeSsn: '123456789',
+                caseNum: '11223344',
+                caseTypeCd: 'AF',
+                medInsurOrderedFlag: 'Y',
+                nmsnCountryCd: 'US',
+                nmsnStateCd: 'GA',
+                courtName: 'Eleventh Circuit',
+                courtOrderIdent: '11111111',
+                courtOrderDt: new Date(),
+            //   employeeAdweNtePct;
+                employeeCsoMaxPremium: '600',
+                nmsnReferralNmsnResponseList: {},
+                nmsnReferralNmsnreferCoverageList: {},
+                nmsnReferralNmsnreferDependentList: {},
+                nmsnReferralNmsnreferPersorgList: {},
+            },
+            { 
+                //   docTrackingNum
+                nmsnReferralReportTypeCd: 'MN',
+                nmsnReferralStatusCd: 'COMPLETED',
+                employerFein: '12121234',
+                employeeSsn: '987654321',
+                caseNum: '00001111',
+                caseTypeCd: 'NA',
+                medInsurOrderedFlag: 'N',
+                nmsnCountryCd: 'US',
+                nmsnStateCd: 'GA',
+                courtName: 'Eleventh Circuit',
+                courtOrderIdent: '1',
+                courtOrderDt: new Date(),
+                employeeAdweNtePct: 15,
+                employeeCsoMaxPremium: '500',
+                nmsnReferralNmsnResponseList: {},
+                nmsnReferralNmsnreferCoverageList: {},
+                nmsnReferralNmsnreferDependentList: {},
+                nmsnReferralNmsnreferPersorgList: {},
+            },
+        ] 
+    )
+    res.end();
 });
 
 app.post('/NmsnrespSubmissionService', function(req, res) {
