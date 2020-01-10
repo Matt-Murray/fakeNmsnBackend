4252f8c24b0d7ea000c4ce3198cd0893b4692285
Clyde <cbates@rfdinc.com>
2020-01-10 13:59:59 -0600

    Start dealing with POSTS. Not functional yet.

--- index.js
+++ index.js
@@ -1,75 +1,103 @@
 // server.js
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 const startingNmsnReferralList = [
-{ 
-    employerFein: '12121234',
-    employeeSsn: '123456789',
-    caseNum: '11223344',
-    caseTypeCd: 'AF',
-    medInsurOrderedFlag: 'Y',
-    nmsnCountryCd: 'US',
-    nmsnStateCd: 'GA',
-    courtName: 'Eleventh Circuit',
-    courtOrderId: '11111111',
-    courtOrderDt: new Date(),
-    employeeAdweNtePct: 0,
-    employeeCsoMaxPremium: 0,
-    insuranceStatusCd: 'ACTIVE',
-    planStatusCd: 'ACTIVE',
-    eligibilityDt: new Date(),
-    nonCustodialName: 'Vader, Darth',
-    nmsnReferralReportTypeCd: 'MN',
-    nmsnReferralStatusCd: 'ACCEPTED',
-    nmsnReferralNmsnResponseList: [''],
-    nmsnReferralNmsnreferCoverageList: [''],
-    nmsnReferralNmsnreferDependentList: [''],
-    nmsnReferralNmsnreferPersorgList: [''],
-},
-{ 
-    employerFein: '12121234',
-    employeeSsn: '987654321',
-    caseNum: '2222222',
-    caseTypeCd: 'AF',
-    medInsurOrderedFlag: 'Y',
-    nmsnCountryCd: 'US',
-    nmsnStateCd: 'GA',
-    courtName: 'Eleventh Circuit',
-    courtOrderId: '33333',
-    courtOrderDt: new Date(),
-    employeeAdweNtePct: 0,
-    employeeCsoMaxPremium: 0,
-    insuranceStatusCd: 'ACTIVE',
-    planStatusCd: 'ACTIVE',
-    eligibilityDt: new Date('July 21, 2019 01:15:00'),
-    nonCustodialName: 'Yorke, Thom',
-    nmsnReferralReportTypeCd: 'MN',
-    nmsnReferralStatusCd: 'ACCEPTED',
-    nmsnReferralNmsnResponseList: [],
-    nmsnReferralNmsnreferCoverageList: [],
-    nmsnReferralNmsnreferDependentList: [],
-    nmsnReferralNmsnreferPersorgList: [],
-}
+    {
+        employerFein: '12121234',
+        employeeSsn: '123456789',
+        caseNum: '11223344',
+        caseTypeCd: 'AF',
+        medInsurOrderedFlag: 'Y',
+        nmsnCountryCd: 'US',
+        nmsnStateCd: 'GA',
+        courtName: 'Eleventh Circuit',
+        courtOrderId: '11111111',
+        courtOrderDt: new Date(),
+        employeeAdweNtePct: 0,
+        employeeCsoMaxPremium: 0,
+        insuranceStatusCd: 'ACTIVE',
+        planStatusCd: 'ACTIVE',
+        eligibilityDt: new Date(),
+        nonCustodialName: 'Vader, Darth',
+        nmsnReferralReportTypeCd: 'MN',
+        nmsnReferralStatusCd: 'ACCEPTED',
+        nmsnReferralNmsnResponseList: [],
+        nmsnReferralNmsnreferCoverageList: [{
+            itemValue: "humanaMedicalPMO",
+            itemLabel:  "Humana PMO"
+        },
+        {
+            itemValue: "blueCrossBlueShieldHMO",
+            itemLabel: "Blue Cross Blue Shield HMO"
+        }],
+        nmsnReferralNmsnreferDependentList: [{
+            childName: 'Skywalker, Luke',
+            childSsn: '***-**-1223',
+            childDOB: new Date("May 25, 1977 01:15:00"),
+            childEnrolledFlag: true,
+            childIneligibleFlag: false
+        },
+        {
+            childName: 'Organa, Leia',
+            childSsn: '***-**-1234',
+            childDOB: new Date("May 25, 1977 01:15:00"),
+            childEnrolledFlag: true,
+            childIneligibleFlag: false
+        }],
+        nmsnReferralNmsnreferPersorgList: [],
+    },
+    {
+        employerFein: '12121234',
+        employeeSsn: '987654321',
+        caseNum: '2222222',
+        caseTypeCd: 'AF',
+        medInsurOrderedFlag: 'Y',
+        nmsnCountryCd: 'US',
+        nmsnStateCd: 'GA',
+        courtName: 'Eleventh Circuit',
+        courtOrderId: '33333',
+        courtOrderDt: new Date(),
+        employeeAdweNtePct: 0,
+        employeeCsoMaxPremium: 0,
+        insuranceStatusCd: 'ACTIVE',
+        planStatusCd: 'ACTIVE',
+        eligibilityDt: new Date('July 21, 2019 01:15:00'),
+        nonCustodialName: 'Yorke, Thom',
+        nmsnReferralReportTypeCd: 'MN',
+        nmsnReferralStatusCd: 'ACCEPTED',
+        nmsnReferralNmsnResponseList: [],
+        nmsnReferralNmsnreferCoverageList: [{
+            itemValue: "humanaMedicalPMO",
+            itemLabel:  "Humana PMO"
+        },
+        {
+            itemValue: "blueCrossBlueShieldHMO",
+            itemLabel: "Blue Cross Blue Shield HMO"
+        }],
+        nmsnReferralNmsnreferDependentList: [],
+        nmsnReferralNmsnreferPersorgList: [],
+    }
 ];
 const nmsnResponsePendingList = [
     {
-        nmsnReferralReportTypeCd : '4154',
-        nmsnReferralStatusCd : '1565',
-        submissionUser : 'Dave',
-        docTrackingNum : '11111111',
-        employerFein : '159754345',
-        employeeSsn : '551234567',
-        caseNum : '33351',
-        caseTypeCd : '1122',
-        medInsurOrderedFlag : 'Y',
-        nmsnCountryCd : 'US',
-        nmsnStateCd : 'KY',
-        courtName : 'Twelfth Circuit',
-        courtOrderIdent : '4455115',
-        courtOrderDt : '1/6/2020 2:20:47 PM',
-        employeeAdweNtePct : 13,
-        employeeCsoMaxPremium : '5000'
+        nmsnReferralReportTypeCd: '4154',
+        nmsnReferralStatusCd: '1565',
+        submissionUser: 'Dave',
+        docTrackingNum: '11111111',
+        employerFein: '159754345',
+        employeeSsn: '551234567',
+        caseNum: '33351',
+        caseTypeCd: '1122',
+        medInsurOrderedFlag: 'Y',
+        nmsnCountryCd: 'US',
+        nmsnStateCd: 'KY',
+        courtName: 'Twelfth Circuit',
+        courtOrderIdent: '4455115',
+        courtOrderDt: '1/6/2020 2:20:47 PM',
+        employeeAdweNtePct: 13,
+        employeeCsoMaxPremium: '5000'
 
     }
 ];
@@ -77,17 +105,19 @@ const nmsnResponseHistoricalList = [];
 const nmsnResponseRejectedList = [];
 
 // app.use(forceSSL());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
+
+app.use(bodyParser.json())
 
 // Run the app by serving the static files in the dist directory
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
-  });
+});
 
 app.options('/*', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -95,8 +125,8 @@ app.options('/*', (req, res) => {
     res.status('200').end();
 })
 
-app.get('/NmsnrespRetrievalService', function(req, res) {
-    
+app.get('/NmsnrespRetrievalService', function (req, res) {
+
     if (req.query.responseType === 'HISTORICAL') {
         res.status('200').json(nmsnResponseHistoricalList);
     } else if (req.query.responseType === 'PENDING') {
@@ -104,22 +134,22 @@ app.get('/NmsnrespRetrievalService', function(req, res) {
     } else if (req.query.responseType === 'REJECTED') {
         res.status('200').json(nmsnResponseRejectedList);
     } else {
-        res.status('400').json({'error': 'responseType not recognized'})
+        res.status('400').json({ 'error': 'responseType not recognized' })
     }
     res.end();
 });
 
-app.get('/NmsnreferRetrievalService', function(req, res) {
+app.get('/NmsnreferRetrievalService', function (req, res) {
     res.status('200').json(startingNmsnReferralList);
     res.end();
 });
 
-app.post('/NmsnrespSubmissionService', function(req, res) {
+app.post('/NmsnrespUpdateService', function (req, res) {
+    for (let i = 0; i < startingNmsnReferralList.length; i++) {
+        if (startingNmsnReferralList[i].caseNum === req.body.caseNum) {
 
-});
-
-app.put('/NmsnrespUpdateService', function(req, res) {
-
+        }
+    }
 });
 
 app.listen(8080, (err) => {
